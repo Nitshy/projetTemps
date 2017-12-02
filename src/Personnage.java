fa85e81e6744e1e69451d35fa9a921f8203b38f0
Shiny <saing.shiny@outlook.fr>
2017-12-02 18:47:59 +0100

    1st commit

---
+++ src/Personnage.java
@@ -0,0 +1,28 @@
+
+import java.time.LocalDate;
+import javafx.util.Duration;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author KingJoe
+ */
+public class Personnage {
+    private String nom;
+    private LocalDate dateNaissance;
+    private Duration tempsVie;;
+    private int chance;
+    private int force;
+    
+    public Personnage(){
+        
+        
+        
+    }
+    
+}
